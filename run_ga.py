b2a83c634e1ec4acf5ee3822191502927e43b433
Kristin Ludwicki <krl4005@med.cornell.edu>
2023-10-26 18:23:13 -0400

    running ga with rrc try/except addition

--- run_ga.py
+++ run_ga.py
@@ -5,9 +5,7 @@ import os
 from supercell_ga import start_ga
 
 def main():
-    #all_individuals = start_ga(pop_size = 200, max_generations=200, path_to_model='./models/', upper_bound = 10, lower_bound = 0.1, save_data_to=os.environ['save_name'], multithread='yes')
-    all_individuals = start_ga(pop_size = 200, max_generations=200, path_to_model='./models/', upper_bound = 10, lower_bound = 0.1, save_data_to='../test_', multithread='no')
-
+    all_individuals = start_ga(pop_size = 200, max_generations=200, path_to_model='./models/', upper_bound = 10, lower_bound = 0.1, save_data_to=os.environ['save_name'], multithread='yes')
     return(all_individuals)
 
 if __name__ == '__main__':
